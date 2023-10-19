3515016b88a281154e2ff25e47e27f54fe92e1c7
Taisiia Zhdanovych <taisiya.zhdan@gmail.com>
2023-10-19 14:44:26 +0300

    add road in index.js

--- server/index.js
+++ server/index.js
@@ -88,6 +88,24 @@ if (process.env.NODE_ENV === 'production') {
   //   res.header('Access-Control-Allow-Credentials', true);
   //   return next();
   // });
+  app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://online-internet-shop-tz.vercel.app');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
